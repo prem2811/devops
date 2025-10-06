3550a672ac79128cc3ed8ae6a6628fd7f57e09ef
prem2811 <premsprem1999@gmail.com>
2025-10-07 01:11:46 +0530

    second

--- hello.java
+++ hello.java
@@ -1,8 +1,10 @@
-class hello
+public class hello
 
 {
     public static void main(args[] String)
     {
-        System.out.println("hello world")
+        System.out.println("hello world");
+        System.out.println("hello world1");
+
     }
 }
