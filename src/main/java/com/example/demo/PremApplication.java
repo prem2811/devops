daefc32a5be23fb048b8e534818b4e01d3a1dbd5
prem2811 <91237989+prem2811@users.noreply.github.com>
2025-10-08 14:50:45 +0530

    Update PremApplication.java

--- src/main/java/com/example/demo/PremApplication.java
+++ src/main/java/com/example/demo/PremApplication.java
@@ -2,13 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PremApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PremApplication.class, args);
-		System.out.println("hello");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PremApplication.class, args);
+        System.out.println("Application started successfully!");
+    }
 
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello from Spring Boot running on Kubernetes!";
+    }
 }
